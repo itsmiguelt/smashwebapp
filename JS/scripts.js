baf5fa1b9c07976aa2962144443fa60f60d20f03
Jack Avery <44183081+OxymoronicTautology@users.noreply.github.com>
2021-04-13 21:25:31 +1000

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,2 +1,2 @@
-var cars = ["Mario", "Donkey Kong", "Link", "Samus", "Dark Samus", "Yoshi", "Kirby", "Captain Falcon"];
+var cars = ["Mario", "Donkey Kong", "Link", "Samus", "Dark Samus", "Yoshi", "Kirby", "Fox", "Pikachu", "Luigi", "Ness", "Captain Falcon", "Jigglypuff", "Peach", "Daisy", "Bowser", "Ice Climbers", "Sheik", ];
 document.getElementById("demo").innerHTML = cars[0];
