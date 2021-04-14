36d7f3e2b34b7aae87e9b286b637e62d9f192856
itsmiguelt <47877854+itsmiguelt@users.noreply.github.com>
2021-04-14 18:52:42 +1000

    trying to get button to work

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,16 +1,20 @@
+var index = 1;
+var characterchar = 0;
 var chars = ["Mario", "Donkey Kong", "Link", "Samus", "Dark Samus", "Yoshi", "Kirby", "Fox", "Pikachu", "Luigi", "Ness", "Captain Falcon", "Jigglypuff", "Peach", "Daisy", "Bowser", "Ice Climbers", "Sheik", "Zelda", "Dr. Mario", "Pichu", "Falco", "Marth", "Lucina", "Young Link", "Ganondorf", "Mewtwo", "Roy", "Chrom", "Mr. Game & Watch", "Meta Knight", "Pit", "Dark Pit", "Zero Suit Samus", "Wario", "Snake", "Ike", "Pokemon Trainer", "Diddy Kong", "Lucas", "Sonic", "King Dedede", "Olimar", "Lucario", "R.O.B.", "Toon Link", "Wolf", "Villager", "Mega Man", "Wii Fit Trainer", "Rosalina & Luma", "Little Mac", "Greninja", "Palutena", "Pac-Man", "Robin", "Shulk", "Boswer Jr.", "Duck Hunt Duo", "Ryu", "Ken", "Cloud", "Corrin", "Bayonetta", "Inkling", "Ridley", "Simon", "Richter", "King K. Rool", "Isabelle", "Incineroar", "Piranha Plant", "Joker", "Hero", "Banjo & Kazooie", "Terry", "Byleth", "Min Min", "Steve", "Sephiroth", "Pyra & Mythra"];
-document.getElementById("demo").innerHTML = cars[0];
-
 
 function Calculatecharacter() {
-   var index = 1;
-   var characterchar = 0;
    while (index < 76) {
         if (document.getElementById(index).checked) {
             characterchar = document.getElementById(index).value;
+            console.log(index)
         } else {
-    index = index + 1
-    print index
+    index = index + 1;
    }
+  }
+  var x = document.getElementById("changes")
+  if (x.innerHTML === "Your next character is:") {
+    x.innerHTML = "Your next character is:" + chars[index];
+  } else {
+    x.innerHTML = "Your next character is";
   }
 }
