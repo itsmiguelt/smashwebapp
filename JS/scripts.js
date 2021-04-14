6f73984dd6271bbf3effdf588784666fdb5178a0
Jack Avery <44183081+OxymoronicTautology@users.noreply.github.com>
2021-04-14 19:50:50 +1000

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,7 @@
 var characters = ["Mario", "Donkey Kong", "Link", "Samus", "Dark Samus", "Yoshi", "Kirby", "Fox", "Pikachu", "Luigi", "Ness", "Captain Falcon", "Jigglypuff", "Peach", "Daisy", "Bowser", "Ice Climbers", "Sheik", "Zelda", "Dr. Mario", "Pichu", "Falco", "Marth", "Lucina", "Young Link", "Ganondorf", "Mewtwo", "Roy", "Chrom", "Mr. Game & Watch", "Meta Knight", "Pit", "Dark Pit", "Zero Suit Samus", "Wario", "Snake", "Ike", "Pokemon Trainer", "Diddy Kong", "Lucas", "Sonic", "King Dedede", "Olimar", "Lucario", "R.O.B.", "Toon Link", "Wolf", "Villager", "Mega Man", "Wii Fit Trainer", "Rosalina & Luma", "Little Mac", "Greninja", "Palutena", "Pac-Man", "Robin", "Shulk", "Boswer Jr.", "Duck Hunt Duo", "Ryu", "Ken", "Cloud", "Corrin", "Bayonetta", "Inkling", "Ridley", "Simon", "Richter", "King K. Rool", "Isabelle", "Incineroar", "Piranha Plant", "Joker", "Hero", "Banjo & Kazooie", "Terry", "Byleth", "Min Min", "Steve", "Sephiroth", "Pyra & Mythra", "You Win!", "You Win!", "You Win!", "You Win!", "You Win!", "You Win!", "You Win!", "You Win!", "You Win!", "You Win!", "You Win!"];
 
 
-function myFunction() {
+function calculate() {
             var ele = document.getElementsByName('char');
             var kill = document.getElementById('kills').value;
             var spike = document.getElementById('spikes').value;
